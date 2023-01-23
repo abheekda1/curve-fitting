136fbae5253d8f1f50513b7035dfd4c3744d5c28
Abheek Dhawan <abheekd@protonmail.com>
2023-01-23 16:39:47 -0600

    feat: add function to get determinant of a matrix

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -9,6 +9,8 @@ public:
     data = new T[columns * rows];
   }
 
+  matrix(int rows, int columns, T data[]) : rows(rows), columns(columns), data(data) {}
+
   int getRows() { return rows; }
   int getColumns() { return columns; }
 
@@ -18,6 +20,36 @@ public:
 
   void insertVal(std::pair<int, int> position, T value) {
     data[position.second * columns + position.first] = value;
+  }
+
+  bool isSquare() { return rows == columns; }
+
+  int getDeterminant() {
+    if (!isSquare()) throw std::runtime_error("cannot get determinant of a non-square matrix");
+
+    if (rows == 2) {
+      return (this->getVal(std::make_pair(0, 0)) * this->getVal(std::make_pair(1, 1)))
+            - (this->getVal(std::make_pair(0, 1)) * this->getVal(std::make_pair(1, 0)));
+    }
+
+    int ret = 0;
+
+    for (int i = 0; i < columns; i++) {
+        matrix<T> subMatrix(rows - 1, columns - 1);
+        int currentColumn = 0;
+        for (int j = 0; j < columns; j++) {
+            if (j == i) continue;
+            int currentRow = 0;
+            for (int k = 1; k < rows; k++) {
+                subMatrix.insertVal(std::make_pair(currentColumn, currentRow), this->getVal(std::make_pair(j, k)));
+                currentRow++;
+            }
+            currentColumn++;
+        }
+        ret += (this->getVal(std::make_pair(i, 0)) * (subMatrix.getDeterminant() * (i % 2 == 0 ? 1 : -1)));
+    }
+
+    return ret;
   }
 
 private:
--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-int main() {}
+#include <iostream>
+
+#include "matrix.hpp"
+
+int main() {
+  int data[] = {
+      4, 3, 2, 2,
+      0, 1, -3, 3,
+      0, -1, 3, 3,
+      0, 3, 1, 1
+  };
+  cf::matrix<int> m(4, 4, data);
+  std::cout << m.getDeterminant() << std::endl;
+}
